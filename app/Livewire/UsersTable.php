d5db8a35409a255391c73242e9325005c351d597
Ho Hong Yen <yenvt9a1@gmail.com>
2023-12-18 12:09:53 +0700

    Perpage and Search

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersTable extends Component
 {
+    use WithPagination;
+
+    public $perPage = 5;
+    public $search = "";
     public function render()
     {
-        return view('livewire.users-table');
+        return view('livewire.users-table', [
+            'users' => User::search($this->search)->paginate($this->perPage),
+        ]);
     }
 }
