dd4aaea4603240e821c5c42e09a77820e843f7eb
Ho Hong Yen <yenvt9a1@gmail.com>
2023-12-18 11:35:54 +0700

    Init project

---
+++ app/Livewire/UsersTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UsersTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.users-table');
+    }
+}
